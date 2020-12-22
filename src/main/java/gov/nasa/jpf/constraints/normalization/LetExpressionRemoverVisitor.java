6fa67541295148b55e746e99faaf431f00d24d4e
Oxana Warkentin <oxana.warkentin@tu-dortmund.de>
2020-12-22 21:15:13 +0100

    handling of LetExpressions needed for nnf

---
+++ src/main/java/gov/nasa/jpf/constraints/normalization/LetExpressionRemoverVisitor.java
@@ -0,0 +1,36 @@
+package gov.nasa.jpf.constraints.normalization;
+
+import gov.nasa.jpf.constraints.api.Expression;
+import gov.nasa.jpf.constraints.expressions.IfThenElse;
+import gov.nasa.jpf.constraints.expressions.LogicalOperator;
+import gov.nasa.jpf.constraints.expressions.Negation;
+import gov.nasa.jpf.constraints.expressions.PropositionalCompound;
+import gov.nasa.jpf.constraints.util.DuplicatingVisitor;
+
+public class LetExpressionRemoverVisitor extends
+        DuplicatingVisitor<Void> {
+
+    private static final LetExpressionRemoverVisitor INSTANCE = new LetExpressionRemoverVisitor();
+
+    public static LetExpressionRemoverVisitor getInstance(){
+        return INSTANCE;
+    }
+
+    @Override
+    public Expression<?> visit(IfThenElse expr, Void data) {
+        Expression ifCond = expr.getIf();
+        Expression thenExpr = expr.getThen();
+        Expression elseExpr = expr.getElse();
+
+        Expression firstPart = PropositionalCompound.create(Negation.create(ifCond), LogicalOperator.OR, thenExpr);
+        Expression secondPart = PropositionalCompound.create(ifCond, LogicalOperator.OR, elseExpr);
+
+        //visit again for finding nested IfThenElse
+        Expression result = PropositionalCompound.create(
+                (Expression<Boolean>) visit(firstPart, data),
+                LogicalOperator.AND,
+                visit(secondPart, data));
+
+        return result;
+    }
+}
